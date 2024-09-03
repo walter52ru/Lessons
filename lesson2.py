b48e117a0d3cb6dd6e7e81b032494631553899f2
master <vek-it@mail.ru>
2024-09-03 21:40:58 +0300

    2.09

--- lesson2.py
+++ lesson2.py
@@ -1,1 +1,5 @@
-print("123")
+count_completed_h_a = 12
+count_hours_spent = 1.5
+course_name = 'Python'
+time_one_task = count_hours_spent / count_completed_h_a
+print ('Курс:',course_name,', всего задач:',count_completed_h_a,', затрачено часов:',count_hours_spent,', среднее время выполения ',time_one_task,' часа', sep='')
