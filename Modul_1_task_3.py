1cad0aef9132b4b5ffcf086989dc8091dfd4a5b7
master <vek-it@mail.ru>
2024-09-06 11:29:35 +0300

    2.09

--- Modul_1_task_3.py
+++ Modul_1_task_3.py
@@ -2,7 +2,7 @@ name = 'Vladimir'
 print ('Имя:',name)
 age = 39
 print ('Возраст:',age)
-new_age = 6
-print ('Новый возраст:',age + new_age)
+age = age + 6
+print ('Новый возраст:',age)
 is_student = True
 print ('Студент:',is_student)
