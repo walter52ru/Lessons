a675f09152072aec4458af8c008f5e66736e0471
master <vek-it@mail.ru>
2024-09-06 17:30:17 +0300

    2.09

--- Modul_2_task_2.py
+++ Modul_2_task_2.py
@@ -3,7 +3,7 @@ second = int(input())
 third = int(input())
 if first == second and second == third:
     print(3)
-elif first == second or second == third or  first == third:
+elif first == second or second == third or first == third:
     print(2)
 else:
     print(0)
