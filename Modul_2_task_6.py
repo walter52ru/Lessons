7cfa1ae5117fe6d0fab7ef376c490036e467f228
master <vek-it@mail.ru>
2024-09-08 12:08:57 +0300

    2.09

--- Modul_2_task_6.py
+++ Modul_2_task_6.py
@@ -3,7 +3,7 @@ for a in range(1,30):
     i = random.randint(3,20)
     s = str(i) + ' '
     for j in range(1,i):
-        for k in range(j + 1, i):
+        for k in range(j, i):
             if i % (j + k) == 0 and j != k:
                 s += (str(j) + str(k))
     print(s)
