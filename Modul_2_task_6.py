6be5884726dad64e96246751a55d2696f7441296
master <vek-it@mail.ru>
2024-09-08 12:08:38 +0300

    2.09

--- Modul_2_task_6.py
+++ Modul_2_task_6.py
@@ -1,9 +1,9 @@
 import random
 for a in range(1,30):
     i = random.randint(3,20)
-    s = str(i)+' '
+    s = str(i) + ' '
     for j in range(1,i):
-        for k in range(j,i):
-            if i % (j+k) == 0 and j != k:
+        for k in range(j + 1, i):
+            if i % (j + k) == 0 and j != k:
                 s += (str(j) + str(k))
     print(s)
